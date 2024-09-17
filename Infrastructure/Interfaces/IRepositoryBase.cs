bc63e6f6b51119018bc59f6b3fd5df9d1cabfa22
RB <rkrb@tyfonmail.se>
2024-09-17 14:47:02 +0200

    CRUD IRepositoryBase.cs & RepositoryBase.cs

--- Infrastructure/Interfaces/IRepositoryBase.cs
+++ Infrastructure/Interfaces/IRepositoryBase.cs
@@ -1,6 +1,11 @@
-namespace Data;
+namespace Infrastructure.Interfaces;
 
-public interface IRepositoryBase
+public interface IRepositoryBase<T> where T : class
 {
-    
+    Task<IEnumerable<T>> GetAllAsync();
+
+    Task<T?> GetByIdAsync(Guid id);
+    Task CreateAsync(T entity);
+    void UpdateAsync(T entity);
+    void DeleteAsync(T entity);
 }
