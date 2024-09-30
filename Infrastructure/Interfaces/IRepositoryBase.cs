0d70c9de91bf39e84f6d8bd600ec6d2dd5cf9c1b
Viktor Näslund <1299835+Raiu@users.noreply.github.com>
2024-09-30 10:49:28 +0200

    Squashed commit

--- Infrastructure/Interfaces/IRepositoryBase.cs
+++ Infrastructure/Interfaces/IRepositoryBase.cs
@@ -1,14 +1,22 @@
 using System.Linq.Expressions;
-
-using Microsoft.EntityFrameworkCore;
+using Infrastructure.Models;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 
 namespace Infrastructure.Interfaces;
 
-public interface IRepositoryBase<T> where T : class
+public interface IRepositoryBase<T>
+    where T : class
 {
-    public IQueryable<T> GetAll();
+    IQueryable<T> GetQuery(
+        IEnumerable<Expression<Func<T, bool>>>? filters = null,
+        ICollection<SortParams>? sorting = null,
+        PageParams? paging = null,
+        IEnumerable<Expression<Func<T, object>>>? includes = null
+    );
     IQueryable<T> GetByConditionAsync(Expression<Func<T, bool>> expression);
-    void UpdateAsync(T entity);
-    void DeleteAsync(T entity);
-    Task CreateAsync(T entity);
+    EntityEntry<T> Update(T entity);
+    EntityEntry<T> Delete(T entity);
+    Task<EntityEntry<T>> CreateAsync(T entity);
+    IEnumerable<T> GetAll();
+    Task<IEnumerable<T>> GetAllAsync();
 }
---
+++ Infrastructure/Models/PageParams.cs
@@ -0,0 +1,9 @@
+namespace Infrastructure.Models;
+
+#nullable disable
+
+public class PageParams
+{
+    public int Page { get; set; }
+    public int Size { get; set; }
+}
